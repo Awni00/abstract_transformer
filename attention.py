c0f11867d15c85b8a8d480a33196d571501ae1a1
awni <awnyaltabaa@gmail.com>
2024-03-16 14:43:39 -0400

    added new attention module -- custom implementation with additional featurees

---
+++ attention.py
@@ -0,0 +1,264 @@
+"""
+An implementation of attention including several additional features and customizations over the standard pytorch implementation.
+"""
+
+import torch
+from torch import nn
+import blocks
+import math
+from typing import Any, Optional, Tuple
+
+class Attention(nn.Module):
+    def __init__(self,
+            d_model: int,
+            n_heads: int,
+            dropout: float,
+            n_kv_heads: int = None,
+            activation: str = 'softmax',
+            add_bias_kv: bool = False,
+            add_bias_out: bool = False,
+            total_n_heads: int = None):
+        """
+        An implementation of Attention with some added customization.
+
+        Allows multi-query attention/grouped query attention, rotary positional embeddings,
+        and custom relation activation functions.
+
+        Parameters
+        ----------
+        d_model : int
+            model dimension
+        n_heads : int
+            number of heads (query heads if n_kv_heads is set)
+        dropout : float
+            dropout rate
+        n_kv_heads : int, optional
+            number of key/value heads. used to implement multi-query attention or grouped query attention.
+            n_kv_heads=1 corresponds to MQA, n_kv_heads > 1 corresponsd to grouped query attention.
+            n_kv_heads=n_heads is standard MHA. uses MHA when None. By default None
+        activation : str, optional
+            name of activation function applied to attention scores. If softmax, flash attention is used.
+            Otherwise, attention is computed 'manually' with the chosen activation function. By default 'softmax'.
+        apply_rope : bool, optional
+            whether to apply rotary positional embeddings, by default False
+        add_bias_kv : bool, optional
+            whether to use bias in key/value projections, by default False
+        add_bias_out : bool, optional
+            whether to use bias in out projection, by default False
+        total_n_heads : int, optional
+            total number of heads in abstract attention (if using abstract attention).
+            used to ensure that concat(A, E) is of dimension d_model after concatentation.
+            hence, output dimension is (d_model // total_heads) * n_heads.
+            if None, total_heads = n_heads and output dimension is d_model
+        """
+
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads # number of heads (for query)
+        self.n_kv_heads = n_heads if n_kv_heads is None else n_kv_heads # n_kv_heads = 1 corresponds to multi-query attn
+        self.activation = activation # "relation activation function"
+        self.activation_ = blocks.get_activation_function(activation)
+        self.dropout = dropout
+        self.add_bias_kv = add_bias_kv
+        self.add_bias_out = add_bias_out
+        self.total_n_heads = n_heads if total_n_heads is None else total_n_heads
+
+        self.n_rep_kv = self.n_heads // self.n_kv_heads # use same kv heads for several query heads
+        self.head_dim = self.d_model // self.total_n_heads # dim of projections
+        assert self.n_heads % self.n_kv_heads == 0 # make sure n_kv_heads fits into n_heads (i.e., can be grouped)
+        assert self.n_rep_kv * self.n_kv_heads == self.n_heads
+        assert self.total_n_heads * self.head_dim == self.d_model
+
+        self.attn_scale = 1 / math.sqrt(self.head_dim) # for scaled dot product attention
+
+        self.wq = nn.Linear(self.d_model, self.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(self.d_model, self.n_kv_heads * self.head_dim, bias=self.add_bias_kv)
+        self.wv = nn.Linear(self.d_model, self.n_kv_heads * self.head_dim, bias=self.add_bias_kv)
+        self.wo = nn.Linear(self.n_heads * self.head_dim, self.n_heads * self.head_dim, bias=self.add_bias_out)
+        self.attn_dropout = nn.Dropout(self.dropout)
+        self.resid_dropout = nn.Dropout(self.dropout)
+
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        freqs_cos: torch.Tensor = None,
+        freqs_sin: torch.Tensor = None,
+        attn_mask: torch.Tensor = None, # boolean attention mask: True indicates corresponding position *should* be attended to
+        is_causal: bool = False, # indicates causal mask; should only set one of is_causal and attn_mask
+        need_weights: bool = False
+    ):
+        """
+        compute attention with given query, key, value.
+
+        if freqs_cos and freqs_sin are given, apply rotary positional embeddings.
+        if attn_mask is given, apply attention mask.
+        if is_causal is True, apply causal mask (attn_mask must be None).
+
+        Parameters
+        ----------
+        query : torch.Tensor
+            query sequence of shape [bsz, len_in, d_model]
+        key : torch.Tensor
+            key sequence of shape [bsz, len_ctx, d_model]
+        value : torch.Tensor
+            value sequence of shape [bsz, len_ctx, d_model]
+        freqs_cos : torch.Tensor, optional
+            cosine of frequencies for RoPE. RoPE is applied if given.
+            Note: RoPE does not work for cross-attention. By default None
+        freqs_sin : torch.Tensor, optional
+            cosine of frequencies for RoPE. RoPE is applied if given.
+            Note: RoPE does not work for cross-attention. By default None
+        attn_mask : torch.Tensor, optional
+            boolean attention mask of shape [len_in, len_ctx]. True at [i,j] indicates i is allowed to attend to j.
+            By default None
+        is_causal : bool, optional
+            whether to apply a causal mask. If True, attn_mask must be None. Only applies for self-attention.
+            By default False
+        need_weights : bool, optional
+            whether to return the attention scores. If True, return value will be tuple (output, attn_scores).
+            If True, will compute attention manually rather than using flash attention. By default False
+
+        Returns
+        -------
+        torch.Tensor
+            result of attention
+        """
+
+        bsz, qseqlen, _ = query.shape
+        bsz, kseqlen, _ = key.shape
+        bsz, vseqlen, _ = value.shape
+        assert kseqlen == vseqlen
+
+        # apply query/key/value projections and reshape to split into different heads
+        xq, xk, xv = self.wq(query), self.wk(key), self.wv(value)
+        xq = xq.view(bsz, qseqlen, self.n_heads, self.head_dim)
+        xk = xk.view(bsz, kseqlen, self.n_kv_heads, self.head_dim)
+        xv = xv.view(bsz, vseqlen, self.n_kv_heads, self.head_dim)
+
+        # apply RoPE relative positional embeddings (if given)
+        if freqs_cos is not None and freqs_sin is not None:
+            xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
+
+        # grouped multiquery attention: expand out keys and values
+        if self.n_rep_kv != 1:
+            xk = repeat_kv(xk, self.n_rep_kv)  # (bs, seqlen, n_heads, head_dim)
+            xv = repeat_kv(xv, self.n_rep_kv)  # (bs, seqlen, n_heads, head_dim)
+
+        # make heads into a batch dimension
+        xq = xq.transpose(1, 2)  # (bs, n_heads, seqlen, head_dim)
+        xk = xk.transpose(1, 2)
+        xv = xv.transpose(1, 2)
+
+        # use flash implementation for softmax activation if weights not needed
+        if self.activation == 'softmax' and not need_weights:
+            output = torch.nn.functional.scaled_dot_product_attention(
+                xq, xk, xv, attn_mask=attn_mask, dropout_p=self.dropout if self.training else 0.0, is_causal=is_causal, scale=self.attn_scale)
+            scores = None
+
+        # manual implementation for other kinds of activation
+        else:
+            # generate causal attn_mask
+            assert not (attn_mask is not None and is_causal)
+            if is_causal and attn_mask is None:
+                attn_mask = compute_causal_mask(qseqlen, device=xq.device)
+                # better to pass attn_mask rather than compute so that it doesn't need to be computed at each layer?
+
+            # compute dot product
+            scores = torch.matmul(xq, xk.transpose(2, 3)) * self.attn_scale
+
+            # if softmax activation, masking is handled by adding -inf before softmax
+            if attn_mask is not None and self.activation == 'softmax':
+                attn_mask_ = torch.zeros(qseqlen, kseqlen, dtype=xq.dtype).masked_fill(attn_mask.logical_not(), float('-inf'))
+                scores = scores + attn_mask_
+
+            # apply (relation) activation to inner products
+            scores = self.activation_(scores)
+
+            # for non-softmax activation, masking is handled by zero-ing out *after* activation
+            if attn_mask is not None and self.activation != 'softmax':
+                scores = scores * attn_mask
+
+            scores = self.attn_dropout(scores)
+            output = torch.matmul(scores, xv)  # (bs, n_local_heads, seqlen, head_dim)
+
+        # restore time as batch dimension and concat heads
+        output = output.transpose(1, 2).contiguous().view(bsz, qseqlen, -1)
+
+        # final projection into the residual stream
+        output = self.wo(output)
+        output = self.resid_dropout(output)
+
+        if need_weights:
+            return output, scores
+
+        return output
+
+def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
+    bs, slen, n_kv_heads, head_dim = x.shape
+    if n_rep == 1:
+        return x
+    return (
+        x[:, :, :, None, :]
+        .expand(bs, slen, n_kv_heads, n_rep, head_dim)
+        .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
+    )
+
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)  # type: ignore
+    freqs = torch.outer(t, freqs).float()  # type: ignore
+    freqs_cos = torch.cos(freqs)  # real part
+    freqs_sin = torch.sin(freqs)  # imaginary part
+    return freqs_cos, freqs_sin
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(shape)
+
+def apply_rotary_emb(
+    xq: torch.Tensor,
+    xk: torch.Tensor,
+    freqs_cos: torch.Tensor,
+    freqs_sin: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor]:
+
+    # reshape xq and xk to match the complex representation
+    xq_r, xq_i = xq.float().reshape(xq.shape[:-1] + (-1, 2)).unbind(-1)
+    xk_r, xk_i = xk.float().reshape(xk.shape[:-1] + (-1, 2)).unbind(-1)
+
+    # reshape freqs_cos and freqs_sin for broadcasting
+    freqs_cos = reshape_for_broadcast(freqs_cos, xq_r)
+    freqs_sin = reshape_for_broadcast(freqs_sin, xq_r)
+
+    # apply rotation using real numbers
+    xq_out_r = xq_r * freqs_cos - xq_i * freqs_sin
+    xq_out_i = xq_r * freqs_sin + xq_i * freqs_cos
+    xk_out_r = xk_r * freqs_cos - xk_i * freqs_sin
+    xk_out_i = xk_r * freqs_sin + xk_i * freqs_cos
+
+    # flatten last two dimensions
+    xq_out = torch.stack([xq_out_r, xq_out_i], dim=-1).flatten(3)
+    xk_out = torch.stack([xk_out_r, xk_out_i], dim=-1).flatten(3)
+
+    return xq_out.type_as(xq), xk_out.type_as(xk)
+
+# NOTE: be careful. pytorch API is inconsistent about whether True means attend or not attend. 
+# this works with the Attention module implemented above, but will only be compatible with some but not all pytorch implementations
+# e.g., works with nn.functional.scaled_dot_product_attention but not nn.MultiHeadAttention
+def compute_diag_mask(size, device=None):
+    """computes an attention mask with False on the diagonal and True elsewhere"""
+
+    diag_mask = torch.eye(size, device=device).logical_not()
+    # diag_mask = diag_mask.masked_fill(diag_mask == 1, float('-inf'))
+    return diag_mask
+
+def compute_causal_mask(size, device=None):
+    """computes an attention mask with True at (i,j) if i <= j"""
+    causal_mask = torch.tril(torch.ones(size, size, device=device)).bool()
+    return causal_mask
