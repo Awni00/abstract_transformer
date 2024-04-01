eff464e331b9453d2851b11271db4b076a7f772e
awni <awnyaltabaa@gmail.com>
2024-04-01 19:03:06 -0400

    renamed

--- transformer_blocks.py
+++ transformer_blocks.py
@@ -217,21 +217,3 @@ class FeedForwardBlock(nn.Module):
         return x
 
 
-def get_activation_function(name):
-    """gets activation function by its name."""
-
-    activation_dict = {
-        'relu': nn.ReLU(),
-        'sigmoid': nn.Sigmoid(),
-        'tanh': nn.Tanh(),
-        'gelu': nn.GELU(),
-        'silu': nn.SiLU(),
-        'softmax': nn.Softmax(dim=-1),
-        'identity': nn.Identity(),
-        # add more if needed
-    }
-    if name in activation_dict:
-        return activation_dict[name]
-    else:
-        raise ValueError(f'Activation function {name} not found in {activation_dict.keys()}')
-
