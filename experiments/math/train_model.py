1834e5bf1cc8dcd5429e8bc003852560477f0198
Awni <awnyaltabaa@gmail.com>
2024-04-02 20:12:51 -0400

    updated for abstract attention and disentangled RCA

--- experiments/math/train_model.py
+++ experiments/math/train_model.py
@@ -18,11 +18,12 @@ from seq2seq_models import Seq2SeqAbstractTransformer, Seq2SeqTransformer
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--task', required=True, type=str, help='task name')
-parser.add_argument('--ee', required=True, type=int, help='number of encoder self-attention heads')
-parser.add_argument('--ea', required=True, type=int, help='number of encoder relational cross-attention heads')
-parser.add_argument('--de', required=True, type=int, help='number of decoder self-attention heads')
-parser.add_argument('--da', required=True, type=int, help='number of decoder relational cross-attention heads')
-parser.add_argument('--dc', required=True, type=int, help='number of decoder cross-attention heads')
+parser.add_argument('--e_sa', required=True, type=int, help='number of encoder self-attention heads')
+parser.add_argument('--e_rca', required=True, type=int, help='number of encoder relational cross-attention heads')
+parser.add_argument('--d_sa', required=True, type=int, help='number of decoder self-attention heads')
+parser.add_argument('--d_rca', required=True, type=int, help='number of decoder relational cross-attention heads')
+parser.add_argument('--d_cross', required=True, type=int, help='number of decoder cross-attention heads')
+parser.add_argument('--disentangled', required=True, type=int, help="wehther to use disentangled RCA (0 or 1)")
 parser.add_argument('--e_n_layers', required=True, type=int, help='number of encoder layers')
 parser.add_argument('--d_n_layers', required=True, type=int, help='number of decoder layers')
 parser.add_argument('--d_model', required=True, type=int, help='model dimension')
@@ -42,11 +43,12 @@ batch_size = args.batch_size
 n_epochs = args.n_epochs
 task = args.task
 
-ee, ea, de, da, dc = args.ee, args.ea, args.de, args.da, args.dc
+e_sa, e_rca, d_sa, d_rca, d_cross = args.e_sa, args.e_rca, args.d_sa, args.d_rca, args.d_cross
 e_n_layers = args.e_n_layers
 d_n_layers = args.d_n_layers
+use_disentangled_rca = bool(args.disentangled)
 
-group_name = f'ee={ee}; ea={ea}; de={de}; da={da}; dc={dc}; el={e_n_layers}; dl={d_n_layers}'
+group_name = f'e_sa={e_sa}; e_rca={e_rca}; d_sa={d_sa}; d_rca={d_rca}; d_cross={d_cross}; el={e_n_layers}; dl={d_n_layers}'
 run_name = args.run_name
 
 # region some configuration
@@ -178,8 +180,10 @@ model_args = dict(
     input_spec=dict(type='token', vocab_size=vocab_size), output_spec=dict(type='token', vocab_size=vocab_size),
     symbol_retrieval='sym_attn', symbol_retrieval_kwargs=dict(model_dim=d_model, n_heads=4, num_symbols=256, dropout=0.0),
     d_model=d_model, out_dim=vocab_size, n_layers_enc=e_n_layers, n_layers_dec=d_n_layers,
-    encoder_kwargs=dict(n_heads_enc=ee, n_heads_abs=ea, dff=dff, activation='relu', norm_first=False, dropout_rate=0.1, causal=False, rel_mask_diag=False),
-    decoder_kwargs=dict(n_heads_enc=de, n_heads_abs=da, n_heads_cross=dc, dff=dff, activation='relu', norm_first=False, dropout_rate=0.1, causal=True, rel_mask_diag=False),
+    encoder_kwargs=dict(n_heads_sa=e_sa, n_heads_rca=e_rca, rca_disentangled=use_disentangled_rca,
+        dff=dff, activation='relu', norm_first=False, dropout_rate=0.1, causal=False, rel_mask_diag=False),
+    decoder_kwargs=dict(n_heads_sa=d_sa, n_heads_rca=d_rca, n_heads_cross=d_cross, rca_disentangled=use_disentangled_rca,
+        dff=dff, activation='relu', norm_first=False, dropout_rate=0.1, causal=True, rel_mask_diag=False),
     in_block_size=max_q_len, out_block_size=max_a_len, loss_ignore_idx=empty_token)
 model = Seq2SeqAbstractTransformer(**model_args)#.to(device)
 torchinfo.summary(model, row_settings=["depth", "var_names"], col_names=["num_params", "params_percent", "trainable"], depth=3, col_width=20)
