18a5c2059b94859145e70abaeff7ec617964cbcc
awni <awnyaltabaa@gmail.com>
2024-05-29 17:57:51 -0400

    updated module according to refactoring of dual_attn_blocks + added documentation

--- vision_models.py
+++ vision_models.py
@@ -1,34 +1,69 @@
-"""Implementation of Vision Transformer (ViT) and Vision Abstract Transformer (VAT)"""
+"""Implementation of Vision Transformer (ViT) and Vision Dual Attention Transformer (ViDAT)"""
 
 import torch
 import torch.nn as nn
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
+from typing import Tuple
 
-from abstract_blocks import AbstractEncoderBlock
+from dual_attn_blocks import DualAttnEncoderBlock
 from symbol_retrieval import (
     PositionalSymbolRetriever, PositionRelativeSymbolRetriever, RelationalSymbolicAttention, SymbolicAttention)
 from transformer_blocks import EncoderBlock
 
 
-class ViT(nn.Module):
+class VisionTransformer(nn.Module):
     """Vision Transformer"""
     def __init__(self,
-        image_shape,
-        patch_size,
-        num_classes,
-        d_model,
-        n_layers,
-        n_heads,
-        dff,
-        dropout_rate,
-        activation,
-        norm_first,
-        norm_type='layernorm',
-        bias=True,
-        pool='cls'):
+        image_shape: Tuple[int],
+        patch_size: Tuple[int],
+        num_classes: int,
+        d_model: int,
+        n_layers: int,
+        n_heads: int,
+        dff: int,
+        dropout_rate: float,
+        activation: str,
+        norm_first: bool,
+        norm_type: str = 'layernorm',
+        bias: bool = True,
+        pool: str = 'cls'):
+        """
+        Vision Transformer.
 
-        super(ViT, self).__init__()
+
+        Parameters
+        ----------
+        image_shape : Tuple[int]
+            shape of image (channels, width, height)
+        patch_size : Tuple[int]
+            size of patch (width, height)
+        num_classes : int
+            number of classes
+        d_model : int
+            model dimension
+        n_layers : int
+            number of layers
+        n_heads : int
+            number of attention heads
+        dff : int
+            feedforward dimension
+        dropout_rate : float
+            dropout rate
+        activation : str
+            name of activation function in feedforward blocks
+        norm_first : bool
+            whether to apply normalization before or after attention. norm_first=True means pre-norm otherwise post-norm.
+        norm_type : 'layernorm' or 'rmsnorm', optional
+            type of normalization to use, by default 'layernorm'
+        bias : bool, optional
+            whether to use a bias in the encoder blocks, by default True
+        pool : 'cls' or 'mean', optional
+            type of pooling to use before final class prediction. 'cks' corresponds to using a class token
+            while 'mean' corresponds to mean pooling, by default 'cls'
+        """
+
+        super(VisionTransformer, self).__init__()
         self.img_channels, self.img_width, self.img_height = image_shape
         self.patch_width, self.patch_height = patch_size
         self.num_classes = num_classes
@@ -98,29 +133,73 @@ class ViT(nn.Module):
         return x
 
 
-class VAT(nn.Module):
-    """Vision Abstract Transformer"""
+class VisionDualAttnTransformer(nn.Module):
+    """Vision Dual Attention Transformer"""
     def __init__(self,
-        image_shape,
-        patch_size,
-        num_classes,
-        d_model,
-        n_layers,
-        n_heads_sa,
-        n_heads_rca,
-        dff,
-        dropout_rate,
-        activation,
-        norm_first,
-        symbol_retrieval,
-        symbol_retrieval_kwargs,
-        rca_type='standard',
-        rca_kwargs=None,
-        bias=True,
-        norm_type='layernorm',
-        pool='cls'):
+        image_shape: Tuple[int],
+        patch_size: Tuple[int],
+        num_classes: int,
+        d_model: int,
+        n_layers: int,
+        n_heads_sa: int,
+        n_heads_ra: int,
+        dff: int,
+        dropout_rate: float,
+        activation: str,
+        norm_first: bool,
+        symbol_retrieval: str,
+        symbol_retrieval_kwargs: dict,
+        ra_type: str = 'relational_attention',
+        ra_kwargs: dict = None,
+        norm_type: str = 'layernorm',
+        bias: bool = True,
+        pool: str = 'cls'):
+        """
+        Vision Transformer.
 
-        super(VAT, self).__init__()
+
+        Parameters
+        ----------
+        image_shape : Tuple[int]
+            shape of image (channels, width, height)
+        patch_size : Tuple[int]
+            size of patch (width, height)
+        num_classes : int
+            number of classes
+        d_model : int
+            model dimension
+        n_layers : int
+            number of layers
+        n_heads_sa : int
+            number of self-attention heads
+        n_heads_ra : int
+            number of relational attention heads
+        dff : int
+            feedforward dimension
+        dropout_rate : float
+            dropout rate
+        activation : str
+            name of activation function in feedforward blocks
+        norm_first : bool
+            whether to apply normalization before or after attention. norm_first=True means pre-norm otherwise post-norm.
+        symbol_retrieval : str
+            type of symbol retrieval mechanism to use, one of 'sym_attn', 'rel_sym_attn', 'pos_sym_retriever', 'pos_relative'
+        symbol_retrieval_kwargs : dict
+            keyword arguments for symbol retrieval mechanism
+        ra_type : 'relational_attention', 'rca', or 'disrca', optional
+            type of relational attention module (e.g., whether to use RCA for an ablation experiment), by default 'relational_attention'
+        ra_kwargs : dict, optional
+            relational attention kwargs, by default None
+        norm_type : 'layernorm' or 'rmsnorm', optional
+            type of normalization to use, by default 'layernorm'
+        bias : bool, optional
+            whether to use a bias in the encoder blocks, by default True
+        pool : 'cls' or 'mean', optional
+            type of pooling to use before final class prediction. 'cks' corresponds to using a class token
+            while 'mean' corresponds to mean pooling, by default 'cls'
+        """
+
+        super(VisionDualAttnTransformer, self).__init__()
         self.img_channels, self.img_width, self.img_height = image_shape
         self.patch_width, self.patch_height = patch_size
         self.num_classes = num_classes
@@ -135,15 +214,15 @@ class VAT(nn.Module):
         self.d_model = d_model
         self.n_layers = n_layers
         self.n_heads_sa = n_heads_sa
-        self.n_heads_rca = n_heads_rca
+        self.n_heads_ra = n_heads_ra
         self.dff = dff
         self.dropout_rate = dropout_rate
         self.activation = activation
         self.norm_first = norm_first
         self.norm_type = norm_type
         self.bias = bias
-        self.rca_type = rca_type
-        self.rca_kwargs = rca_kwargs if rca_kwargs is not None else {}
+        self.rca_type = ra_type
+        self.rca_kwargs = ra_kwargs if ra_kwargs is not None else {}
         self.symbol_retrieval = symbol_retrieval
 
         if symbol_retrieval == 'sym_attn':
@@ -173,10 +252,10 @@ class VAT(nn.Module):
         self.cls_token = nn.Parameter(torch.randn(1, 1, self.d_model))
         self.dropout = nn.Dropout(self.dropout_rate)
 
-        self.encoder_blocks = nn.ModuleList([AbstractEncoderBlock(
-            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_rca=n_heads_rca, dff=dff, dropout_rate=dropout_rate,
+        self.encoder_blocks = nn.ModuleList([DualAttnEncoderBlock(
+            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_ra=n_heads_ra, dff=dff, dropout_rate=dropout_rate,
             activation=activation, norm_first=norm_first, norm_type=norm_type, bias=bias, causal=False,
-            rca_type=self.rca_type, rca_kwargs=self.rca_kwargs)
+            ra_type=self.rca_type, ra_kwargs=self.rca_kwargs)
             for _ in range(n_layers)])
 
         self.final_out = nn.Linear(self.d_model, self.num_classes)
