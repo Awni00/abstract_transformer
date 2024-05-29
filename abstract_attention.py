e823afcc0b428fb9325cd484faaab23a5091f5cf
awni <awnyaltabaa@gmail.com>
2024-05-29 16:57:08 -0400

    updated module name to names used in paper + added doccumentation

--- abstract_attention.py
+++ abstract_attention.py
@@ -2,35 +2,75 @@ import torch
 import torch.nn as nn
 
 from attention import Attention
-from relational_cross_attention import RelationalCrossAttention, DisentangledRelationalCrossAttention, DisentangledRelationalCrossAttentionV2
+from relational_attention import RelationalCrossAttention, DisentangledRelationalCrossAttention, RelationalAttention
 
-class AbstractAttention(nn.Module):
+# An implementation of Dual Attention as proposed in the paper
+# "Disentangling and Integrating Relational and Sensory Information in Transformer Architectures"
+# Awni Altabaa, John Lafferty (2024). https://arxiv.org/abs/2405.16727
+
+# The DualAttention module is a form of multi-head attention involving a composition of two distinct types of attention heads.
+# The first type is standard self-attention, which captures object-level (i.e., sensory) features, and
+# the second type is relational attention, which captures relational features.
+
+# DualAttention is a concatenation of self-attention and relational attention heads.
+
+class DualAttention(nn.Module):
     def __init__(self,
         d_model: int,
         n_heads_sa: int,
-        n_heads_rca: int,
+        n_heads_ra: int,
         dropout: float,
         sa_kwargs: dict = None,
-        rca_kwargs: dict = None,
-        rca_type: str = 'standard'
+        ra_kwargs: dict = None,
+        ra_type: str = 'standard'
     ):
-        super(AbstractAttention, self).__init__()
+        """An implementation of Dual Attention.
+
+        The DualAttention module is a form of multi-head attention involving a composition of two distinct types of attention heads.
+        The first type is standard self-attention, which captures object-level (i.e., sensory) features, and
+        the second type is relational attention, which captures relational features.
+
+
+        Parameters
+        ----------
+        d_model : int
+            model dimension
+        n_heads_sa : int
+            number of self-attention heads
+        n_heads_ra : int
+            number of relational attention heads
+        dropout : float
+            dropout rate
+        sa_kwargs : dict, optional
+            self-attention kwargs, by default None
+        ra_kwargs : dict, optional
+            relational attention kwargs, by default None
+        ra_type : str, optional
+            type of relational attention module (e.g., whether to use RCA for an ablation experiment).
+            by default 'relational_attention'.
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
+        super(DualAttention, self).__init__()
         self.d_model = d_model
         self.n_heads_sa = n_heads_sa
-        self.n_heads_rca = n_heads_rca
+        self.n_heads_ra = n_heads_ra
         self.dropout = dropout
         self.sa_kwargs = sa_kwargs if sa_kwargs is not None else {}
-        self.rca_kwargs = rca_kwargs if rca_kwargs is not None else {}
-        self.rca_type = rca_type
+        self.ra_kwargs = ra_kwargs if ra_kwargs is not None else {}
+        self.ra_type = ra_type
 
         self.use_self_attn = n_heads_sa > 0
-        self.use_rca = n_heads_rca > 0
+        self.use_rel_attn = n_heads_ra > 0
 
 
-        self.total_n_heads = n_heads_sa + n_heads_rca
+        self.total_n_heads = n_heads_sa + n_heads_ra
 
-        if not (self.use_self_attn or self.use_rca):
-            raise ValueError("At least one of self-attention or relational cross-attention must be used")
+        if not (self.use_self_attn or self.use_rel_attn):
+            raise ValueError("At least one of self-attention or relational attention must be used")
 
         if self.use_self_attn:
             self.self_attention = Attention(
@@ -38,21 +78,23 @@ class AbstractAttention(nn.Module):
                 total_n_heads=self.total_n_heads, dropout=dropout,
                 **self.sa_kwargs)
 
-        if self.use_rca and rca_type=='standard':
-            self.relational_cross_attention = RelationalCrossAttention(
-                d_model=d_model, n_heads=n_heads_rca,
+        if self.use_rel_attn and ra_type=='relational_attention':
+            self.relational_attention = RelationalAttention(
+                d_model=d_model, n_heads=n_heads_ra,
                 total_n_heads=self.total_n_heads, dropout=dropout,
-                **self.rca_kwargs)
-        elif self.use_rca and rca_type=='disentangled_v1':
-            self.relational_cross_attention = DisentangledRelationalCrossAttention(
-                d_model=d_model, n_heads=n_heads_rca,
+                **self.ra_kwargs)
+        elif self.use_rel_attn and ra_type=='rca':
+            self.relational_attention = RelationalCrossAttention(
+                d_model=d_model, n_heads=n_heads_ra,
                 total_n_heads=self.total_n_heads, dropout=dropout,
-                **self.rca_kwargs)
-        elif self.use_rca and rca_type=='disentangled_v2':
-            self.relational_cross_attention = DisentangledRelationalCrossAttentionV2(
-                d_model=d_model, n_heads=n_heads_rca,
+                **self.ra_kwargs)
+        elif self.use_rel_attn and ra_type=='disrca':
+            self.relational_attention = DisentangledRelationalCrossAttention(
+                d_model=d_model, n_heads=n_heads_ra,
                 total_n_heads=self.total_n_heads, dropout=dropout,
-                **self.rca_kwargs)
+                **self.ra_kwargs)
+        else:
+            raise ValueError(f"Invalid relational attention type: {ra_type}")
 
 
     def forward(
@@ -75,21 +117,21 @@ class AbstractAttention(nn.Module):
                 need_weights=need_weights)
 
         # relational cross-attention
-        if self.use_rca:
-            rca_out, *rca_scores = self.relational_cross_attention(
+        if self.use_rel_attn:
+            rel_attn_out, *rel_attn_scores = self.relational_attention(
                 x, symbols,
                 attn_mask=attn_mask, is_causal=is_causal,
                 freqs_cos=freqs_cos, freqs_sin=freqs_sin)
 
         # combine self-attention and relational cross-attention
-        if self.use_rca and self.use_self_attn:
+        if self.use_rel_attn and self.use_self_attn:
             # concat self-attention output (E) and relational cross-attention output (A)
-            out = torch.concat((self_attn_out, rca_out), dim=-1)
-        elif self.use_rca:
-            out = rca_out # only use relational cross-attention
+            out = torch.concat((self_attn_out, rel_attn_out), dim=-1)
+        elif self.use_rel_attn:
+            out = rel_attn_out # only use relational cross-attention
             self_attn_scores = None
         elif self.use_self_attn:
             out = self_attn_out # only use standard self-attention
-            rca_scores = None
+            rel_attn_scores = None
 
-        return out, self_attn_scores, rca_scores
+        return out, self_attn_scores, rel_attn_scores
