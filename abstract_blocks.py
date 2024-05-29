740dcd7b058a010c95826a964aaf46425c2d0a4d
awni <awnyaltabaa@gmail.com>
2024-05-29 17:10:09 -0400

    updated module names to match paper + updated documentation

--- abstract_blocks.py
+++ abstract_blocks.py
@@ -1,77 +1,81 @@
 import torch
 import torch.nn as nn
-from abstract_attention import AbstractAttention
+from dual_attention import DualAttention
 from attention import Attention
 from transformer_blocks import FeedForwardBlock, create_norm
 
-# TODO: update docstrings
-class AbstractEncoderBlock(nn.Module):
+class DualAttnEncoderBlock(nn.Module):
 
     def __init__(self,
             d_model: int,
             n_heads_sa: int,
-            n_heads_rca: int,
+            n_heads_ra: int,
             dff: int,
-            dropout_rate: float,
             activation: str,
+            dropout_rate: float,
             norm_first: bool,
             norm_type: str = 'layernorm',
             sa_kwargs: dict = None,
-            rca_kwargs: dict = None,
-            rca_type: str = 'standard',
-            rel_mask_diag: bool = True,
+            ra_kwargs: dict = None,
+            ra_type: str = 'relational_attention',
             bias: bool = True,
             causal: bool = False):
         """
-        Abstract Encoder Block.
+        Dual Attention Encoder Block.
 
-        An Abstract Encoder is a variant of the Transformer Encoder that uses a mixture of specialized attention heads.
-        Some attention heads are standard self-attention heads, while other heads are relational cross-attention heads.
+        A Dual Attention Encoder is a variant of the Transformer Encoder that uses a combination of two distinct types of attention heads.
+        The first type is standard self-attention, which captures object-level (i.e., sensory) features, and
+        the second type is relational attention, which captures relational features.
 
         Parameters
         ----------
         d_model : int
             model dimension.
         n_heads_sa : int
-            number of standard self-attention heads
-        n_heads_rca : int
-            number of "abstract" relational cross-attention heads
+            number of standard self-attention heads.
+        n_heads_ra : int
+            number of relational attention heads.
         dff : int
             intermediate dimension of feed-forward block.
+        activation : str
+            name of activation function to use in feedforward block.
         dropout_rate : float
             dropout rate.
-        activation : str
-            name of activation function to use in feed-forward block.
         norm_first : bool
-            whether to apply layer normalization before or after attention.
-        rel_mask_diag : bool, optional
-            whether to mask out self-relations in relational cross-attention, by default True
+            whether to apply normalization before or after attention. norm_first=True means pre-norm otherwise post-norm.
+        norm_type : 'layernorm' or 'rmsnorm, optional
+            type of normalization to use, by default 'layernorm'
+        sa_kwargs : dict, optional
+            self-attention kwargs, by default None
+        ra_kwargs : dict, optional
+            relational attention kwargs, by default None
+        ra_type : str, optional
+            type of relational attention module (e.g., whether to use RCA for an ablation experiment), by default 'relational_attention'
         bias : bool, optional
             whether to use bias in multi-head attention, by default True
         causal : bool, optional
-            whether self-attention should be causal, by default False
+            whether attention operations should be causal, by default False
         """
 
         super().__init__()
         self.d_model = d_model
         self.n_heads_sa = n_heads_sa
-        self.n_heads_abs = n_heads_rca
+        self.n_heads_ra = n_heads_ra
         self.dff = dff
         self.dropout_rate = dropout_rate
         self.activation = activation
         self.norm_first = norm_first
         self.norm_type = norm_type
-        self.rca_type = rca_type
-        self.rel_mask_diag = rel_mask_diag
+        self.rca_type = ra_type
         self.bias = bias
         self.causal = causal
 
         self.dropout = nn.Dropout(self.dropout_rate)
         self.norm1 = create_norm(self.d_model, self.norm_type)
-        self.abstract_attn = AbstractAttention(
-            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_rca=n_heads_rca,
-            dropout=dropout_rate, sa_kwargs=sa_kwargs, rca_kwargs=rca_kwargs,
-            rca_type=rca_type)
+        self.dual_attn = DualAttention(
+            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_ra=n_heads_ra,
+            dropout=dropout_rate, sa_kwargs=sa_kwargs, ra_kwargs=ra_kwargs,
+            ra_type=ra_type)
 
         self.norm2 = create_norm(self.d_model, self.norm_type)
         self.ff_block = FeedForwardBlock(self.d_model, dff=self.dff, activation=self.activation, use_bias=self.bias)
@@ -79,17 +83,17 @@ class AbstractEncoderBlock(nn.Module):
     # TODO: make attn_mask input so it only needs to be computed once?
     def forward(self, x, symbols, freqs_cos=None, freqs_sin=None):
         if self.norm_first:
-            x = x + self._compute_abstract_attn(self.norm1(x), symbols, freqs_cos=freqs_cos, freqs_sin=freqs_sin)
+            x = x + self._compute_dual_attn(self.norm1(x), symbols, freqs_cos=freqs_cos, freqs_sin=freqs_sin)
             x = x + self._apply_ff_block(self.norm2(x))
         else:
-            x = self.norm1(x + self._compute_abstract_attn(x, symbols, freqs_cos=freqs_cos, freqs_sin=freqs_sin))
+            x = self.norm1(x + self._compute_dual_attn(x, symbols, freqs_cos=freqs_cos, freqs_sin=freqs_sin))
             x = self.dropout(x)
             x = self.norm2(x + self._apply_ff_block(x))
         return x
 
-    def _compute_abstract_attn(self, x, symbols, freqs_cos=None, freqs_sin=None):
+    def _compute_dual_attn(self, x, symbols, freqs_cos=None, freqs_sin=None):
 
-        x, *_ = self.abstract_attn(x, symbols,
+        x, *_ = self.dual_attn(x, symbols,
             need_weights=False, is_causal=self.causal,
             freqs_cos=freqs_cos, freqs_sin=freqs_sin)
 
@@ -102,110 +106,89 @@ class AbstractEncoderBlock(nn.Module):
         x = self.dropout(x)
         return x
 
-    # NOTE: for now, diag_mask is ignored...
-    # def _compute_rel_attn_mask(self, x):
-    #     size = x.size(1)
-    #     mask = torch.zeros(size=(size, size), device=x.device)
-    #     if self.rel_mask_diag:
-    #         mask += compute_diag_mask(size, device=x.device)
-    #     if self.causal:
-    #         mask += torch.nn.modules.transformer.Transformer.generate_square_subsequent_mask(size, device=x.device)
 
-    #     # edge case: if both diagonal mask and causal mask, the all elements of the first row will be -inf
-    #     # this becomes NaN after softmax, so we set it to 0
-    #     if self.rel_mask_diag and self.causal:
-    #         mask[0,0] = 0.0
-
-    #     if self.rel_mask_diag or self.causal:
-    #         return mask
-    #     else:
-    #         return None
-
-
-    # def _compute_self_attn_mask(self, x):
-    #     if self.causal:
-    #         causal_mask = torch.nn.modules.transformer.Transformer.generate_square_subsequent_mask(x.size(1), device=x.device)
-    #         return causal_mask
-    #     else:
-    #         return None
-
-class AbstractDecoderBlock(nn.Module):
+class DualAttnDecoderBlock(nn.Module):
     def __init__(self,
                 d_model: int,
                 n_heads_sa: int,
-                n_heads_rca: int,
+                n_heads_ra: int,
                 n_heads_cross: int,
                 dff: int,
-                dropout_rate: float,
                 activation: str,
+                dropout_rate: float,
                 norm_first: bool,
                 norm_type: str = 'layernorm',
                 sa_kwargs: dict = None,
-                rca_kwargs: dict = None,
+                ra_kwargs: dict = None,
                 cross_kwargs: dict = None,
-                rca_type: bool = False,
-                rel_mask_diag: bool = True,
+                ra_type: str = 'relational_attention',
                 bias: bool = True,
                 causal: bool = True):
         """
-        Abstract Decoder Block.
+        Dual Attention Decoder Block.
 
-        An Abstract Decoder is a variant of the Transformer Decoder that uses a mixture of specialized attention heads.
-        Some attention heads are standard self-attention heads, while other heads are relational cross-attention heads.
+        A Dual Attention Decoder is a variant of the Transformer Decoder that uses a combination of two distinct types of attention heads.
+        The first type is standard self-attention, which captures object-level (i.e., sensory) features, and
+        the second type is relational attention, which captures relational features.
 
         Parameters
         ----------
-        symbol_retriever : nn.Module
-            symbol retriever module. assigns each object in collection of inputs a symbol from a symbol library.
         d_model : int
             model dimension.
         n_heads_sa : int
             number of standard self-attention heads.
-        n_heads_rca : int
-            number of "abstract" relational cross-attention heads.
+        n_heads_ra : int
+            number of relational attention heads.
         n_heads_cross : int
             number of cross-attention heads.
         dff : int
             intermediate dimension of feed-forward block.
+        activation : str
+            name of activation function to use in feedforward block.
         dropout_rate : float
             dropout rate.
-        activation : str
-            name of activation function to use in feed-forward block.
         norm_first : bool
-            whether to apply layer normalization before or after attention.
-        rel_mask_diag : bool, optional
-            whether to mask out self-relations in relational cross-attention, by default True
+            whether to apply normalization before or after attention. norm_first=True means pre-norm otherwise post-norm.
+        norm_type : 'layernorm' or 'rmsnorm, optional
+            type of normalization to use, by default 'layernorm'
+        sa_kwargs : dict, optional
+            self-attention kwargs, by default None
+        ra_kwargs : dict, optional
+            relational attention kwargs, by default None
+        cross_kwargs : dict, optional
+            cross-attention kwargs, by default None
+        ra_type : str, optional
+            type of relational attention module (e.g., whether to use RCA for an ablation experiment), by default 'relational_attention'
         bias : bool, optional
             whether to use bias in multi-head attention, by default True
         causal : bool, optional
-            whether self-attention should be causal, by default False
+            whether attention operations should be causal, by default False
         """
 
         super().__init__()
         self.d_model = d_model
         self.n_heads_sa = n_heads_sa
-        self.n_heads_abs = n_heads_rca
+        self.n_heads_ra = n_heads_ra
         self.n_heads_cross = n_heads_cross
         self.dff = dff
         self.dropout_rate = dropout_rate
         self.activation = activation
         self.norm_first = norm_first
         self.norm_type = norm_type
-        self.rca_type = rca_type
-        self.rel_mask_diag = rel_mask_diag
+        self.ra_type = ra_type
         self.bias = bias
         self.causal = causal
 
         self.use_self_attn = n_heads_sa > 0
-        self.use_abs_attn = n_heads_rca > 0
+        self.use_rel_attn = n_heads_ra > 0
 
         self.dropout = nn.Dropout(self.dropout_rate)
         self.norm1 = create_norm(self.d_model, self.norm_type)
 
-        self.abstract_attn = AbstractAttention(
-            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_rca=n_heads_rca,
-            dropout=dropout_rate, sa_kwargs=sa_kwargs, rca_kwargs=rca_kwargs,
-            rca_type=rca_type)
+        self.dual_attn = DualAttention(
+            d_model=d_model, n_heads_sa=n_heads_sa, n_heads_ra=n_heads_ra,
+            dropout=dropout_rate, sa_kwargs=sa_kwargs, ra_kwargs=ra_kwargs,
+            ra_type=ra_type)
 
         self.norm2 = create_norm(self.d_model, self.norm_type)
         cross_kwargs = cross_kwargs if cross_kwargs is not None else {}
@@ -217,18 +200,18 @@ class AbstractDecoderBlock(nn.Module):
 
     def forward(self, x, context, symbols):
         if self.norm_first:
-            x = x + self._compute_abstract_attn(self.norm1(x), symbols)
+            x = x + self._compute_dual_attn(self.norm1(x), symbols)
             x = x + self._compute_cross_attn(self.norm2(x), context)
             x = x + self.ff_block(self.norm3(x))
         else:
-            x = self.norm1(x + self._compute_abstract_attn(x, symbols))
+            x = self.norm1(x + self._compute_dual_attn(x, symbols))
             x = self.norm2(x + self._compute_cross_attn(x, context))
             x = self.norm3(x + self.ff_block(x))
         return x
 
-    def _compute_abstract_attn(self, x, symbols):
+    def _compute_dual_attn(self, x, symbols):
 
-        x, *_ = self.abstract_attn(x, symbols, need_weights=False, is_causal=self.causal)
+        x, *_ = self.dual_attn(x, symbols, need_weights=False, is_causal=self.causal)
 
         x = self.dropout(x) # dropout
 
@@ -239,39 +222,7 @@ class AbstractDecoderBlock(nn.Module):
         x = self.dropout(x)
         return x
 
-    # def _compute_rel_attn_mask(self, x):
-    #     size = x.size(1)
-    #     mask = torch.zeros(size=(size, size), device=x.device)
-    #     if self.rel_mask_diag:
-    #         mask += compute_diag_mask(size, device=x.device)
-    #     if self.causal:
-    #         mask += torch.nn.modules.transformer.Transformer.generate_square_subsequent_mask(size, device=x.device)
-
-    #     # edge case: if both diagonal mask and causal mask, the all elements of the first row will be -inf
-    #     # this becomes NaN after softmax, so we set it to 0
-    #     if self.rel_mask_diag and self.causal:
-    #         mask[0,0] = 0.0
-
-    #     if self.rel_mask_diag or self.causal:
-    #         return mask
-    #     else:
-    #         return None
-
-    # def _compute_self_attn_mask(self, x):
-    #     if self.causal:
-    #         causal_mask = torch.nn.modules.transformer.Transformer.generate_square_subsequent_mask(x.size(1), device=x.device)
-    #         return causal_mask
-    #     else:
-    #         return None
-
     def _apply_ff_block(self, x):
         x = self.ff_block(x)
         x = self.dropout(x)
         return x
-
-# def compute_diag_mask(size, device=None):
-#     """computes an attention mask with -inf on the diagonal and 0 elsewhere"""
-
-#     diag_mask = torch.eye(size, device=device)
-#     diag_mask = diag_mask.masked_fill(diag_mask == 1, float('-inf'))
-#     return diag_mask
