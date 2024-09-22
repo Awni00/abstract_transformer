4d0c97b9bd31b2dc69ba955837c71ef4ec123b01
Awni00 <awnyaltabaa@gmail.com>
2024-09-22 19:07:29 -0400

    updated

--- figure_generation/wandb_utils.py
+++ figure_generation/wandb_utils.py
@@ -8,11 +8,14 @@ def get_wandb_project_table(project_name, entity, attr_cols=('group', 'name'), c
 
     runs = api.runs(entity + "/" + project_name)
 
-    if summary_cols == 'all':
-        summary_cols = set().union(*tuple(run.summary.keys() for run in runs))
-
     if config_cols == 'all':
         config_cols = set().union(*tuple(run.config.keys() for run in runs))
+
+    if summary_cols == 'all':
+        # get all unique keys in the summary of all runs
+        summary_cols = set().union(*tuple(run.summary.keys() for run in runs))
+        # remove keys that also appear in the config
+        summary_cols = {col for col in summary_cols if col not in config_cols}
 
     all_cols = list(attr_cols) + list(summary_cols) + list(config_cols)
     if len(all_cols) > len(set(all_cols)):
